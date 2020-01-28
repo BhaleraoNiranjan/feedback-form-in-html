4edcec6fcbc1a54b2b0de5a9b4dc879fd01f4d59
Niranjan Bhalerao <nirajbhalerao.12@gmail.com>
2020-01-28 14:15:16 +0800

    devopsevent

---
+++ feedback.php
@@ -0,0 +1,19 @@
+<?php
+define('DB_HOST','localhost')
+define('DB_NAME','practice')
+define('DB_USER',root)
+define('DB_PASSWORD','')
+
+$con = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL:".mysql_error());
+$db = mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySql:".mysql_error());
+
+function feedback()
+{
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$rate = $_POST['radio_experience'];
+	$comment = $_POST['comment'];
+	$query = INSERT INTO websiteuser(name,email,rate,comment)values($name,$email,$rate,$comment);
+	mysql_query($query);	
+}
+
